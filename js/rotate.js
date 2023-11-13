349054c1d57de48012424d6d09430fa127664857
akane0825 <94824613+akane0825@users.noreply.github.com>
2023-11-13 11:58:04 +0900

    rotate完成

--- js/rotate.js
+++ js/rotate.js
@@ -2,19 +2,17 @@ var canvas,ctx,photo,comWidth,comHeight,url;
 
 //回転
 //右回転
+var angle = 0;
 var Rrotate = document.getElementById('rightbutton').addEventListener('click',function(){
-    var canvasrotate = document.getElementById('canvas1');
-    var ctx4 = canvasrotate.getContext('2d');
-    photo = new Image();
-    photo.src = url;
-    photo.onload = function(){
-        imageData = ctx.getImageData(0,0,comWidth,comHeight);
-        rotate = ctx.createImageData(comWidth,comHeight);
-        
-        ctx4.putImageData(rotate,0,0);
-    }
+    var crotate = document.getElementById('canvas1');
+    var ctx4 = crotate.getContext('2d');
+    angle+=90;
+    crotate.style.transform = "rotate("+angle+"deg)";
 })
 //左回転
 var Lrotate = document.getElementById('leftbutton').addEventListener('click',function(){
-    
+    var crotate = document.getElementById('canvas1');
+    var ctx4 = crotate.getContext('2d');
+    angle-=90;
+    crotate.style.transform = "rotate("+angle+"deg)";
 })
