0af3aa41714ee5270e23b7a529965ef354209719
akane0825 <94824613+akane0825@users.noreply.github.com>
2023-11-13 10:58:25 +0900

    追記・修正

--- js/gray.js
+++ js/gray.js
@@ -1,40 +1,5 @@
 var canvas,ctx,photo,comWidth,comHeight,url;
 
-//選択された画像ファイルをcanvasに表示
-function onFileSelect(files,canvas1){
-    var element = document.getElementById("filebox");
-    var txt = document.getElementById("text");
-    txt.innerHTML=element.value;
-    canvas = document.getElementById('canvas1');
-    ctx = canvas.getContext('2d');
-    var reader = new FileReader();
-    reader.onload = function(event){
-        photo = new Image();
-        photo.onload = function(){
-            var cwidth = canvas.width;
-            var cheight = canvas.height;
-            var photowidth = photo.naturalWidth;
-            var photoheight = photo.naturalHeight;
-            var canvasratio = cheight /cwidth;
-            var ratio = photoheight / photowidth;
-            if(ratio <= canvasratio){
-                comWidth = cwidth;
-                var comratio = cwidth / photowidth;
-                comHeight = comratio * photoheight;
-            }else{
-                comHeight = cheight;
-                comratio = cheight / photoheight;
-                comWidth = comratio * photowidth;
-            }
-            //canvasに圧縮された画像を表示
-            ctx.drawImage(photo,0,0,comWidth,comHeight);
-        }
-        photo.src = event.target.result;
-        url = photo.src;
-    }
-    reader.readAsDataURL(files[0]);
-}
-
 //グレースケール化
 var gray = document.getElementById('buttongray').addEventListener('click',function(){
     var canvasgray = document.getElementById('canvasgary');
--- js/rotate.js
+++ js/rotate.js
@@ -1,38 +1,20 @@
 var canvas,ctx,photo,comWidth,comHeight,url;
 
-//選択された画像ファイルをcanvasに表示
-function onFileSelect(files,canvas1){
-    var element = document.getElementById("filebox");
-    var txt = document.getElementById("text");
-    txt.innerHTML=element.value;
-    canvas = document.getElementById('canvas1');
-    ctx = canvas.getContext('2d');
-    var reader = new FileReader();
-    reader.onload = function(event){
-        photo = new Image();
-        photo.onload = function(){
-            var cwidth = canvas.width;
-            var cheight = canvas.height;
-            var photowidth = photo.naturalWidth;
-            var photoheight = photo.naturalHeight;
-            var canvasratio = cheight /cwidth;
-            var ratio = photoheight / photowidth;
-            if(ratio <= canvasratio){
-                comWidth = cwidth;
-                var comratio = cwidth / photowidth;
-                comHeight = comratio * photoheight;
-            }else{
-                comHeight = cheight;
-                comratio = cheight / photoheight;
-                comWidth = comratio * photowidth;
-            }
-            //canvasに圧縮された画像を表示
-            ctx.drawImage(photo,0,0,comWidth,comHeight);
-        }
-        photo.src = event.target.result;
-        url = photo.src;
-    }
-    reader.readAsDataURL(files[0]);
-}
 //回転
-//これから
+//右回転
+var Rrotate = document.getElementById('rightbutton').addEventListener('click',function(){
+    var canvasrotate = document.getElementById('canvas1');
+    var ctx4 = canvasrotate.getContext('2d');
+    photo = new Image();
+    photo.src = url;
+    photo.onload = function(){
+        imageData = ctx.getImageData(0,0,comWidth,comHeight);
+        rotate = ctx.createImageData(comWidth,comHeight);
+        
+        ctx4.putImageData(rotate,0,0);
+    }
+})
+//左回転
+var Lrotate = document.getElementById('leftbutton').addEventListener('click',function(){
+    
+})
